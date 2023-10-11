03edd9cc0e485a6983ac4f7bbbb213ba47aa28e2
zied abderrahim <zied.abderrahim@esprit.tn>
2023-10-11 23:46:38 +0200

    newcommit

---
+++ app/src/main/java/tn/esprit/nascar/NewsFragment.kt
@@ -0,0 +1,15 @@
+package tn.esprit.nascar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NewsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+}
